61bc5e4b5852ee1d9f4ee3ce8a7baf78971e6dab
Promise Nwhator <74368087+nwhator@users.noreply.github.com>
2024-01-02 13:34:18 +0100

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-"""Exports TODO list progress of all employees to JSON."""
-import requests
-import json
+"""Exports to-do list information of all employees to JSON format."""
 
-def export_all_employees_to_json():
+import json
+import requests
+
+
+if __name__ == "__main__":
     # API base URL
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Retrieve all users
+    # Retrieve information for all users
     users = requests.get(url + "users").json()
 
-    # Prepare data for JSON
-    data = {}
-    for user in users:
-        user_id = str(user.get("id"))
-        todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    # Define JSON file name
+    json_file_name = "todo_all_employees.json"
 
-        tasks_data = [
-            {"username": user.get("username"), "task": task.get("title"), "completed": task.get("completed")}
-            for task in todos
-        ]
-
-        data[user_id] = tasks_data
-
-    # Write data to JSON file
-    filename = "todo_all_employees.json"
-    with open(filename, 'w') as jsonfile:
-        json.dump(data, jsonfile, indent=2)  # Adding indent for better readability
-
-# Example usage:
-if __name__ == "__main__":
-    # Call the function to export TODO progress of all employees to JSON
-    export_all_employees_to_json()
+    # Export to JSON file
+    with open(json_file_name, "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
