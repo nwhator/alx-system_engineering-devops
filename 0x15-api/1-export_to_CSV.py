c35f3239f7c59741464c8d1294b083f2de84e917
Promise Nwhator <74368087+nwhator@users.noreply.github.com>
2024-01-02 13:09:19 +0100

    Update Indentation W191 fixed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -35,5 +35,5 @@ if __name__ == "__main__":
 
         # Write rows for each task
         [writer.writerow(
-		[user_id, username, t.get("completed"), t.get("title")])
-		for t in todos]
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
