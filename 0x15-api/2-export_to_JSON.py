6ceabbbaede8edaa3c419df4a8d4565ce69a4879
Promise Nwhator <74368087+nwhator@users.noreply.github.com>
2024-01-02 13:29:32 +0100

    Update

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,38 +1,36 @@
 #!/usr/bin/python3
-"""Exports employee TODO list progress to JSON."""
-import requests
+"""Exports to-do list information for a given employee ID to JSON format."""
+
 import json
+import requests
 import sys
 
 
-def export_to_json(employee_id):
+if __name__ == "__main__":
+    # Get employee ID from command-line argument
+    user_id = sys.argv[1]
+
     # API base URL
     url = "https://jsonplaceholder.typicode.com/"
 
     # Retrieve user information
-    user = requests.get(url + "users/{}".format(employee_id)).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
+
+    # Extract username
+    username = user.get("username")
 
     # Retrieve TODO list for the given employee ID
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    # Prepare data for JSON
-    data = {str(user.get("id")): [{"task": task.get("title"), "completed": task.get("completed"), "username": user.get("username")} for task in todos]}
+    # Define JSON file name
+    json_file_name = "{}.json".format(user_id)
 
-    # Write data to JSON file
-    filename = "{}.json".format(employee_id)
-    with open(filename, 'w') as jsonfile:
-        json.dump(data, jsonfile)
-
-
-# Example usage:
-if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    # Parse employee ID from command-line argument
-    employee_id = int(sys.argv[1])
-
-    # Call the function to export employee TODO progress to JSON
-    export_to_json(employee_id)
+    # Export to JSON file
+    with open(json_file_name, "w") as jsonfile:
+        json.dump({
+            user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]
+        }, jsonfile)
