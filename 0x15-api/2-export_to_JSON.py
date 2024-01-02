cba97b5872685be3150a09f4261bbe1d146521c9
Promise Nwhator <74368087+nwhator@users.noreply.github.com>
2024-01-02 13:31:05 +0100

    Update Pycodestyle error fixed

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,36 +1,27 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
+"""Exports to-do list information of all employees to JSON format."""
 
 import json
 import requests
-import sys
 
 
 if __name__ == "__main__":
-    # Get employee ID from command-line argument
-    user_id = sys.argv[1]
-
     # API base URL
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Retrieve user information
-    user = requests.get(url + "users/{}".format(user_id)).json()
-
-    # Extract username
-    username = user.get("username")
-
-    # Retrieve TODO list for the given employee ID
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    # Retrieve information for all users
+    users = requests.get(url + "users").json()
 
     # Define JSON file name
-    json_file_name = "{}.json".format(user_id)
+    json_file_name = "todo_all_employees.json"
 
     # Export to JSON file
     with open(json_file_name, "w") as jsonfile:
         json.dump({
-            user_id: [{
+            u.get("id"): [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
-                "username": username
-            } for t in todos]
-        }, jsonfile)
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
